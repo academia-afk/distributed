a1d55bad1fd939d44b71c14b23fbb5261a1420d1
Pavlo Bukreiev <bukreiev.p.v@nmu.one>
2024-12-16 00:12:54 +0200

    Create create_dataset.py

---
+++ create_dataset.py
@@ -0,0 +1,44 @@
+import fiftyone as fo
+import fiftyone.zoo as foz
+import os
+
+classes = ["Cat"]
+max_samples_train = 80
+max_samples_val = 20
+
+export_dir = "datasets/openimages_coco"
+
+os.makedirs(export_dir, exist_ok=True)
+
+train_dataset = foz.load_zoo_dataset(
+    "open-images-v7",
+    split="train",
+    label_types=["detections"],
+    classes=classes,
+    max_samples=max_samples_train,
+    only_matching=True,
+)
+train_export_dir = os.path.join(export_dir, "train")
+train_dataset.export(
+    export_dir=train_export_dir,
+    dataset_type=fo.types.COCODetectionDataset,
+    label_field="ground_truth",
+)
+
+val_dataset = foz.load_zoo_dataset(
+    "open-images-v7",
+    split="validation",
+    label_types=["detections"],
+    classes=classes,
+    max_samples=max_samples_val,
+    only_matching=True,
+)
+val_export_dir = os.path.join(export_dir, "val")
+val_dataset.export(
+    export_dir=val_export_dir,
+    dataset_type=fo.types.COCODetectionDataset,
+    label_field="ground_truth",
+)
+
+del train_dataset
+del val_dataset
