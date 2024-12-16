0274bb1234e286a427ce3dfb00a426c609580df7
Pavlo Bukreiev <bukreiev.p.v@nmu.one>
2024-12-16 02:29:47 +0200

    Update create_dataset.py

--- create_dataset.py
+++ create_dataset.py
@@ -3,8 +3,8 @@ import fiftyone.zoo as foz
 import os
 
 classes = ["Cat", "Dog", "Person"]
-max_samples_train = 800
-max_samples_val = 200
+max_samples_train = 8
+max_samples_val = 2
 
 export_dir = "datasets/openimages_coco"
 
