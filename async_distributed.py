514d0191365bbae6bcc48cc61717479bea70e2ac
Pavlo Bukreiev <bukreiev.p.v@nmu.one>
2024-12-21 14:46:34 +0200

    Update async_distributed.py

--- async_distributed.py
+++ async_distributed.py
@@ -187,7 +187,7 @@ def train_loop_per_worker(node_id, config, ps):
 
     wandb.init(
         project="async_distributed",
-        group="three_nodes",
+        group="five_nodes",
         name=f"node_{node_id}",
         config=config
     )
@@ -243,7 +243,7 @@ if __name__ == "__main__":
         "train_dir": "/workspace/dataset/training",
         "val_dir":   "/workspace/dataset/validation",
         "num_classes": 20,
-        "num_nodes": 3,
+        "num_nodes": 5,
         "batch_size": 8,
         "num_epochs": 10,
         "lr": 0.005,
